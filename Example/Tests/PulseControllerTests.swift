ff76cdb9cbdce8ccb874315a397d2c242c2ebe6e
Dawid Cieslak <cieslakdawid@gmail.com>
2018-05-09 13:47:47 +0200

    Setup initial test file

---
+++ Example/Tests/PulseControllerTests.swift
@@ -0,0 +1,16 @@
+// https://github.com/Quick/Quick
+
+import Quick
+import Nimble
+import PulseController
+
+class TableOfContentsSpec: QuickSpec {
+    override func spec() {
+        
+        describe("Initial Test") {
+            it("should pass") {
+                expect(1) == 1
+            }
+        }
+    }
+}
